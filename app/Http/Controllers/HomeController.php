a143f34ad89c1db1323e2ca1ab0241e15c65c79a
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-02-06 02:12:05 +0100

    Add routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class HomeController extends Controller
 {
     /**
@@ -21,8 +23,18 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    // public function index()
+    // {
+    //     return view('home');
+    // }
+    public function posts() {
+        $posts = Post::all();
+        return view('pages.posts', compact('posts'));
+    }
+    public function create() {
+        return view('pages.create');
+    }
+    public function store() {
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +20,19 @@ use Illuminate\Support\Facades\Route;
 
 // Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@home')->name('home');
+Route::get('/posts', 'HomeController@posts')->name('posts');
+Route::get('/posts/create', 'HomeController@create')->name('create');
+Route::post('/posts/store', 'HomeController@store')->name('store');
+
+
 Route::get('/', 'GuestController@home') ->name('home');
 
 Route::get('/login', 'Auth\LoginController@showLogin') ->name('show.login');
+Route::post('/login', 'Auth\LoginController@login') ->name('login');
+
+Route::get('/register', 'Auth\RegisterController@showRegister') -> name('show.register');
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
 
