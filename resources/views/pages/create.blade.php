dc1e80aa1ee940e54f4402cd908bf221bc41e4f1
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-02-07 22:41:49 +0100

    Add select of category

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -12,17 +12,24 @@
             <label for="subtitle">Sottotitolo:</label>
             <input type="text" name="subtitle"> <br>
     
-            <label for='author'>Autore:</label>
-            <input type="text" value={{Auth::user()-> name}} name='author'> <br>
+            {{-- <label for='author'>Autore:</label>
+            <input type="text" value={{Auth::user()-> name}} name='author'> <br> --}}
     
-            <label for="text">Testo</label>
-            <input type="text" name="text"> <br>
+            <label for="text">Testo:</label>
+            <textarea type="text" name="text"> </textarea> <br>
     
             <label for="date">Date:</label>
             <input type="date" name="date" >  <br>
 
             <label for="views">Views:</label>
             <input type="number" name="views" >  <br>
+
+            <label for="category_name">Categoria:</label>
+            <select name="category_id" > 
+                @foreach ($categories as $category)
+                    <option value="{{$category -> id}}">{{$category -> category_name }}</option>
+                @endforeach
+            </select> <br>
 
             <input class="btn btn-info" type="submit" value="CREATE">
     
